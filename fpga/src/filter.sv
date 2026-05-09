8480ab48ba378ce34924fc18a66c6a634ddbe630
Cheuk Fung Keith (Chuck) Chow <rosarollins069@gmail.com>
2026-05-09 19:30:00 -0400

    avoids overflow during data_i + prev_q by using a WIDTH+1 bit sum.

--- fpga/src/filter.sv
+++ fpga/src/filter.sv
@@ -3,43 +3,34 @@
 module filter #(
     parameter int unsigned WIDTH = 12
 )(
-    input  logic clk,
-    input  logic rst_n,
+    input  logic             clk,
+    input  logic             rst_n,
     input  logic [WIDTH-1:0] data_i,
-    input  logic valid_i,
+    input  logic             valid_i,
+
     output logic [WIDTH-1:0] data_o,
-    output logic valid_o
+    output logic             valid_o
 );
 
-    logic [WIDTH-1:0] prev_q, prev_d;
-    logic [WIDTH-1:0] filt_q, filt_d;
-    logic valid_d, valid_q;
+    logic [WIDTH-1:0] prev_q;
+    logic [WIDTH:0]   sum;
 
-    always_comb begin
-        filt_d  = filt_q;
-        prev_d  = prev_q;
-        valid_d = 1'b0;
-
-        if (valid_i) begin
-            filt_d  = (data_i + prev_q) >> 1;
-            prev_d  = data_i;
-            valid_d = 1'b1;
-        end
-    end
+    assign sum = {1'b0, data_i} + {1'b0, prev_q};
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             prev_q  <= '0;
-            filt_q  <= '0;
-            valid_q <= 1'b0;
+            data_o  <= '0;
+            valid_o <= 1'b0;
         end else begin
-            prev_q  <= prev_d;
-            filt_q  <= filt_d;
-            valid_q <= valid_d;
+            valid_o <= 1'b0;
+
+            if (valid_i) begin
+                data_o  <= sum[WIDTH:1];
+                prev_q  <= data_i;
+                valid_o <= 1'b1;
+            end
         end
     end
-
-    assign data_o  = filt_q;
-    assign valid_o = valid_q;
 
 endmodule
